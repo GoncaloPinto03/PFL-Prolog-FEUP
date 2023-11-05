f9f792565d3adcc0881513357f10c52f42c6acc7
Guca08 <up202108839@up.pt>
2023-11-05 12:34:04 +0000

    Back to menu working

--- src/game.pl
+++ src/game.pl
@@ -1,100 +1,9 @@
 :-use_module(library(random)).
 :-use_module(library(lists)).
 
-:- consult('board.pl'). % Assuming 'board.pl' contains your board definition
+:- consult('board.pl').
 
-/*
- 
-* * * * * * * * * * FUNCTIONS TO DEVELOP * * * * * * * * * * 
-
-initialState(+GameState)
-
-displayGame(+GameState)
-
-move(+GameState, +Move, -NewGameState)
-
-gameOver(+GameState, -Winner)
-
-validMoves(+GameState, -Moves)
-
-evaluateBoard(+State, -Value)
-
-chooseMove(+GameState, +Level, -Move)
-
- */
-
-/*
-
-PLACE VERSION
-
-game_loop(CurrentPlayer) :-
-    display_board, % Display the current game board
-    display_board_stack, % Display the current game board stack
-    nl, write('Player '), write(CurrentPlayer), write("'s turn."), nl,    
-    
-    get_player_place_play(CurrentPlayer, Row, Column), % Get the player's move
-
-    (valid_place(CurrentPlayer, Row, Column) -> % Check if the move is valid
-        place_piece(Row, Column, CurrentPlayer),
-        (player_wins(CurrentPlayer) -> % Check if the player wins
-            display_board,
-            display_board_stack,
-            write('Player '), write(CurrentPlayer), write(' wins! Game over.'), nl
-        ;
-            % Switch to the other player and continue the game
-            switch_player(CurrentPlayer, NextPlayer),
-            game_loop(NextPlayer)
-        )
-    ;
-        write('Invalid move. Try again.'), nl,
-        game_loop(CurrentPlayer) % Stay on the same player's turn
-    ).
-*/
- 
-/*
-game_loop(CurrentPlayer) :- 
-    display_board,
-    display_board_stack,
-    nl, write('Player '), write(CurrentPlayer), write("'s turn."), nl,
-    
-    get_player_move_play_from(CurrentPlayer, Row1, Column1),
-    get_player_move_play_to(CurrentPlayer, Row2, Column2),
-
-    % Check if the move is valid
-    (valid_move(CurrentPlayer, Row1, Column1) ->
-
-        % Check if the stack heights are equal
-        (equivalence_stack(Row1, Column1, Row2, Column2) ->
-            % Check if the move is to an adjacent cell
-            (is_adjacent_cell(Row1, Column1, Row2, Column2) ->
-                % If all conditions are met, move the piece
-                move_piece(Row1, Column1, Row2, Column2, CurrentPlayer),
-                (player_wins(CurrentPlayer) ->
-                    display_board,
-                    display_board_stack,
-                    write('Player '), write(CurrentPlayer), write(' wins! Game over.'), nl
-                ;
-                    % Switch to the other player and continue the game
-                    switch_player(CurrentPlayer, NextPlayer),
-                    game_loop(NextPlayer)
-                )
-            ;
-                % If the move is not to an adjacent cell, display a message and try again
-                write('Piece can only be moved to an adjacent cell! Try again.'), nl,
-                game_loop(CurrentPlayer)
-            )
-        ;
-            % If the stack heights are not equal, display a message and try again
-            write('Stack heights are not equal! Try again.'), nl,
-            game_loop(CurrentPlayer)
-        )
-    ;
-        write('Invalid move. Try again.'), nl,
-        game_loop(CurrentPlayer)
-    ).
-
-*/
-
+% Game loop for Player vs Player
 game_loop(CurrentPlayer) :-
     display_board,
     display_board_stack,
@@ -104,7 +13,11 @@ game_loop(CurrentPlayer) :-
         write('Player B\'s turn.')
     ),
     nl,
+    write('Type 1 to continue'), nl,
+    write('Type 2 to get back to Menu'), nl,
+    read(Input),
     % Ask the player for their choice: 1 for inserting a piece, 2 for moving a piece
+    (Input =:=1 ->
     write('Enter 1 to insert a piece, 2 to move a piece: '),
     read(PlayerChoice),
 
@@ -173,148 +86,13 @@ game_loop(CurrentPlayer) :-
     ;
         write('Invalid choice. Try again.'), nl,
         game_loop(CurrentPlayer) % Stay on the same player's turn
-    ).
+    )
+   ;
+     menu
+    )    
+.
 
-
-% Define a predicate to get the player's move (row and column)
-get_player_place_play(Player, Row, Column) :-
-    repeat,
-    nl,
-    write('Enter the row (1-8) and column (1-8) where you want to place your piece (e.g., "3 5"): '),
-    read(RowInput),
-    read(ColumnInput),
-    valid_input_place(RowInput, ColumnInput, Row, Column, Player),
-    !. % Cut operator (!) to stop the repeat loop
-
-% Define a predicate to get the player's move (row and column)
-get_player_move_play_from(Player, Row, Column) :-
-    repeat,
-    write('Enter the row (1-8) and column (1-8) from where you want to move your piece (e.g., "3 5"): '),
-    read(RowInput),
-    read(ColumnInput),
-    valid_input_move(RowInput, ColumnInput, Row, Column, Player),
-    !. % Cut operator (!) to stop the repeat loop
-
-% Define a predicate to get the player's move (row and column)
-get_player_move_play_to(Player, Row, Column) :-
-    repeat,
-    write('Enter the row (1-8) and column (1-8) to where you want to move your piece (e.g., "3 5"): '),
-    read(RowInput),
-    read(ColumnInput),
-    switch_player(Player, NextPlayer),
-    valid_input_move(RowInput, ColumnInput, Row, Column, NextPlayer),
-    switch_player(NextPlayer, Player),
-    !. % Cut operator (!) to stop the repeat loop
-
-% Bot move predicate (random move)
-get_bot_place_play(Player, Row, Column) :-
-    repeat,
-    random(0, 8, RandomRow), % Generate a random row (0-7)
-    random(0, 8, RandomCol), % Generate a random column (0-7)
-    valid_place(Player, RandomRow, RandomCol), % Check if the random move is valid
-    Row is RandomRow,
-    Column is RandomCol,
-    !. % Cut operator to stop the repeat loop
-
-% Define a predicate to get the player's move (row and column)
-get_bot_move_play_from(Player, Row, Column) :-
-    repeat,
-    random(0, 8, RandomRow), % Generate a random row (0-7)
-    random(0, 8, RandomCol), % Generate a random column (0-7)
-    valid_input_move(RandomRow, RandomCol, Row, Column, Player),
-    !. % Cut operator (!) to stop the repeat loop
-
-% Define a predicate to get the player's move (row and column)
-get_bot_move_play_to(Player, Row, Column) :-
-    repeat,
-    random(0, 8, RandomRow), % Generate a random row (0-7)
-    random(0, 8, RandomCol), % Generate a random column (0-7)
-    switch_player(Player, NextPlayer),
-    valid_input_move(RandomRow, RandomCol, Row, Column, NextPlayer),
-    switch_player(NextPlayer, Player),
-    !. % Cut operator (!) to stop the repeat loop
-
-% Define a predicate to validate the input
-valid_input_place(RowInput, ColumnInput, Row, Column, Player) :-
-    integer(RowInput),
-    integer(ColumnInput),
-    between(1, 8, RowInput), % Validate row input (1-8)
-    between(1, 8, ColumnInput), % Validate column input (1-8)
-    Row is RowInput - 1, % Adjust for 0-based indexing
-    Column is ColumnInput - 1, % Adjust for 0-based indexing
-    valid_place(Player, Row, Column). % Check if the move is valid based on game rules
-
-% Define a predicate to validate the input
-valid_input_move(RowInput, ColumnInput, Row, Column, Player) :-
-    integer(RowInput),
-    integer(ColumnInput),
-    between(1, 8, RowInput), % Validate row input (1-8)
-    between(1, 8, ColumnInput), % Validate column input (1-8)
-    Row is RowInput - 1, % Adjust for 0-based indexing
-    Column is ColumnInput - 1, % Adjust for 0-based indexing
-    valid_move(Player, Row, Column). % Check if the move is valid based on game rules
-
-
-% Define a predicate to switch players
-switch_player(playerA, playerB).
-switch_player(playerB, playerA).
-
-switch_player(playerA, bot).
-switch_player(bot, playerA).
-
-% Define the win condition check (you need to implement this)
-%player_wins(Player) :- 
-
-% Define a basic valid_place predicate for placing a piece in an empty cell
-valid_place(Player, Row, Column) :-
-    board(Board),
-    nth0(Row, Board, RowList),
-    nth0(Column, RowList, empty),
-    player(Player).
-
-
-valid_move(Player, Row, Column) :-
-    board(Board),
-    nth0(Row, Board, RowList),
-    nth0(Column, RowList, Player),
-    player(Player).
-
-
-/*
-% Game loop for Player A vs Bot
-game_loop_pc(CurrentPlayer) :-
-    display_board,
-    display_board_stack,
-    nl,
-    (CurrentPlayer = playerA ->
-        write('Player A\'s turn.'),
-        % Ask the player for their choice: 1 for inserting a piece, 2 for moving a piece
-        write('Enter 1 to insert a piece, 2 to move a piece: '),
-        read(PlayerChoice),
-        get_player_place_play(CurrentPlayer, Row, Column)
-    ;
-        write('Bot\'s turn.'),
-        random(1, 2, PlayerChoice),
-        get_bot_place_play(Row, Column) % Implement the bot's move predicate
-    ),
-    (valid_place(CurrentPlayer, Row, Column) ->
-        place_piece(Row, Column, CurrentPlayer),
-        (player_wins(CurrentPlayer) -> % Check if the player wins
-            display_board,
-            display_board_stack,
-            write('Player '), write(CurrentPlayer), write(' wins! Game over.'), nl
-        ;
-            % Switch to the other player and continue the game
-            switch_player(CurrentPlayer, NextPlayer),
-            game_loop_pc(NextPlayer)
-        )
-    ;
-        write('Invalid move. Try again.'), nl,
-        game_loop_pc(CurrentPlayer)
-    ).
- */
-
-% Game loop for Player A vs Bot
+% Game loop for Player vs Bot
 game_loop_pc(CurrentPlayer) :-
     display_board,
     display_board_stack,
@@ -402,34 +180,7 @@ game_loop_pc(CurrentPlayer) :-
         game_loop_pc(CurrentPlayer) % Stay on the same player's turn
     ).
 
-/*
-game_loop_cc(CurrentPlayer) :-
-    display_board, % Display the current game board
-    display_board_stack, % Display the current game board stack
-    (CurrentPlayer = playerA ->
-        write('Player A\'s turn.'),
-        get_bot_place_play(CurrentPlayer, Row, Column) % Implement the bot's move predicate
-    ;
-        write('Player B\'s turn.'),
-        get_bot_place_play(CurrentPlayer, Row, Column) % Implement the bot's move predicate
-    ),
-    (valid_place(CurrentPlayer, Row, Column) -> % Check if the move is valid
-        place_piece(Row, Column, CurrentPlayer), % Update the game board
-        (player_wins(CurrentPlayer) -> % Check if the player wins
-            display_board,
-            display_board_stack,
-            write('Player '), write(CurrentPlayer), write(' wins! Game over.'), nl
-        ;
-            % Switch to the other player and continue the game
-            switch_player(CurrentPlayer, NextPlayer),
-            game_loop_cc(NextPlayer)
-        )
-    ;
-        write('Invalid move. Try again.'), nl,
-        game_loop_cc(CurrentPlayer) % Stay on the same player's turn
-    ).
-
-*/
+% Game loop for Player vs Bot
 game_loop_cc(CurrentPlayer) :-
     display_board,
     display_board_stack,
@@ -507,6 +258,106 @@ game_loop_cc(CurrentPlayer) :-
         game_loop_cc(CurrentPlayer) % Stay on the same player's turn
     ).
 
+% Define a predicate to get the player's move (row and column)
+get_player_place_play(Player, Row, Column) :-
+    repeat,
+    nl,
+    write('Enter the row (1-8) and column (1-8) where you want to place your piece (e.g., "3 5"): '),
+    read(RowInput),
+    read(ColumnInput),
+    valid_input_place(RowInput, ColumnInput, Row, Column, Player),
+    !. % Cut operator (!) to stop the repeat loop
+
+% Define a predicate to get the player's move (row and column)
+get_player_move_play_from(Player, Row, Column) :-
+    repeat,
+    write('Enter the row (1-8) and column (1-8) from where you want to move your piece (e.g., "3 5"): '),
+    read(RowInput),
+    read(ColumnInput),
+    valid_input_move(RowInput, ColumnInput, Row, Column, Player),
+    !. % Cut operator (!) to stop the repeat loop
+
+% Define a predicate to get the player's move (row and column)
+get_player_move_play_to(Player, Row, Column) :-
+    repeat,
+    write('Enter the row (1-8) and column (1-8) to where you want to move your piece (e.g., "3 5"): '),
+    read(RowInput),
+    read(ColumnInput),
+    switch_player(Player, NextPlayer),
+    valid_input_move(RowInput, ColumnInput, Row, Column, NextPlayer),
+    switch_player(NextPlayer, Player),
+    !. % Cut operator (!) to stop the repeat loop
+
+% Bot move predicate (random move)
+get_bot_place_play(Player, Row, Column) :-
+    repeat,
+    random(0, 8, RandomRow), % Generate a random row (0-7)
+    random(0, 8, RandomCol), % Generate a random column (0-7)
+    valid_place(Player, RandomRow, RandomCol), % Check if the random move is valid
+    Row is RandomRow,
+    Column is RandomCol,
+    !. % Cut operator to stop the repeat loop
+
+% Define a predicate to get the player's move (row and column)
+get_bot_move_play_from(Player, Row, Column) :-
+    repeat,
+    random(0, 8, RandomRow), % Generate a random row (0-7)
+    random(0, 8, RandomCol), % Generate a random column (0-7)
+    valid_input_move(RandomRow, RandomCol, Row, Column, Player),
+    !. % Cut operator (!) to stop the repeat loop
+
+% Define a predicate to get the player's move (row and column)
+get_bot_move_play_to(Player, Row, Column) :-
+    repeat,
+    random(0, 8, RandomRow), % Generate a random row (0-7)
+    random(0, 8, RandomCol), % Generate a random column (0-7)
+    switch_player(Player, NextPlayer),
+    valid_input_move(RandomRow, RandomCol, Row, Column, NextPlayer),
+    switch_player(NextPlayer, Player),
+    !. % Cut operator (!) to stop the repeat loop
+
+% Define a predicate to validate the input
+valid_input_place(RowInput, ColumnInput, Row, Column, Player) :-
+    integer(RowInput),
+    integer(ColumnInput),
+    between(1, 8, RowInput), % Validate row input (1-8)
+    between(1, 8, ColumnInput), % Validate column input (1-8)
+    Row is RowInput - 1, % Adjust for 0-based indexing
+    Column is ColumnInput - 1, % Adjust for 0-based indexing
+    valid_place(Player, Row, Column). % Check if the move is valid based on game rules
+
+% Define a predicate to validate the input
+valid_input_move(RowInput, ColumnInput, Row, Column, Player) :-
+    integer(RowInput),
+    integer(ColumnInput),
+    between(1, 8, RowInput), % Validate row input (1-8)
+    between(1, 8, ColumnInput), % Validate column input (1-8)
+    Row is RowInput - 1, % Adjust for 0-based indexing
+    Column is ColumnInput - 1, % Adjust for 0-based indexing
+    valid_move(Player, Row, Column). % Check if the move is valid based on game rules
+
+
+% Define a predicate to switch players
+switch_player(playerA, playerB).
+switch_player(playerB, playerA).
+
+switch_player(playerA, bot).
+switch_player(bot, playerA).
+
+
+% Define a basic valid_place predicate for placing a piece in an empty cell
+valid_place(Player, Row, Column) :-
+    board(Board),
+    nth0(Row, Board, RowList),
+    nth0(Column, RowList, empty),
+    player(Player).
+
+
+valid_move(Player, Row, Column) :-
+    board(Board),
+    nth0(Row, Board, RowList),
+    nth0(Column, RowList, Player),
+    player(Player).
 
 
 % Predicate to find the first player piece on the defined edges of the board.
@@ -589,33 +440,4 @@ dfs_left_to_right([Row, Column], Player, Visited, FinalVisited) :-
 % Define the right side spaces of the triangle
 right_side_spaces([[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8]]).
 
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-askTypeOfMove(Player, Row, Column):-
-        write('You want to:'),
-        nl,
-        write('Insert a new letter -> 1'),
-        nl,
-        write('Move a letter to another adjacent board position-> 2'),
-        nl,
-        write('> '),
-        read(Input),
-        read_type_of_move_input(Input, Player, Row, Column).
-
-read_type_of_move_input(1, Player, Row, Column) :- get_player_place_play(Player, Row, Column).
-read_type_of_move_input(2, Player, Row, Column) :- get_player_move_play_from(Player, Row, Column).
-read_type_of_move_input(_Other, Player, Row, Column) :-
-                                        write('You want to:'),
-                                        nl,
-                                        write('Invalid option... Try again!'),
-                                        nl,
-                                        askTypeOfMove(Player, Row, Column).
-                                        
-
-  
- 
+                                    
